68a164c48ef8c99b8c4607ef072c009af92a5bd3
Sebastien Asselin <sebastien@ixmedia.com>
2025-07-17 19:31:01 -0400

    Add more example to template

--- server.ts
+++ server.ts
@@ -21,6 +21,12 @@ serve({
         format: "A4",
         landscape: true,
         printBackground: true,
+        margin: {
+          top: "20mm",
+          bottom: "20mm",
+          left: "10mm",
+          right: "10mm",
+        },
       });
 
       await browser.close();
